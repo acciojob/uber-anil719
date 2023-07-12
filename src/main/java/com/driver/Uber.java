19aa1d45f13e86b4f99e09f6367ac5b6388d0511
Anil <anilterapalli@gmail.com>
2023-07-12 16:28:02 +0530

    uber

--- src/main/java/com/driver/Uber.java
+++ src/main/java/com/driver/Uber.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import io.swagger.v3.oas.annotations.info.Contact;
+
+import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +13,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+
+@OpenAPIDefinition(
+	info = @Info(
+			title = "Uber API Application",
+			version = "1.0.0",
+			description = "This is my Learning Project",
+			termsOfService = "runcodenow",
+			contact = @Contact(
+					name = "Mr Anil Terapalli",
+					email = "anilterapalli@gmail.com"
+			),
+			license = @License(
+					name = "licence",
+					url = "runcodenow"
+			)
+	)
+)
 public class Uber {
 
 	public static void main(String[] args) {
